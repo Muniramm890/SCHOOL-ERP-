68e25307af498ac99bf8f6fe4db4a904577a652b
Mani <muniramm890@gmail.com>
2026-04-30 15:47:31 +0530

    Replace sendError with response in Auth.js

--- src/middleware/Auth.js
+++ src/middleware/Auth.js
@@ -1,7 +1,7 @@
 // src/middleware/auth.js
 const jwt = require('jsonwebtoken');
 const { sql, poolPromise } = require('../config/db'); // poolPromise use karenge
-const { sendError } = require('../utils/response');
+const response = require('../response');
 
 const schema = process.env.DB_SCHEMA || 'whatsapp';
 
