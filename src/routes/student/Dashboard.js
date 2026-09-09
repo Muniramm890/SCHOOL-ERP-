b8d736c3f1e28e5b87e9c63dbeb1aa09a9df95d4
Mani <muniramm890@gmail.com>
2026-09-09 10:41:24 +0530

    Update import paths in Dashboard.js

--- src/routes/student/Dashboard.js
+++ src/routes/student/Dashboard.js
@@ -1,8 +1,8 @@
 //routes/studentDashboard.js
 const router = require('express').Router();
-const ctrl = require('../controllers/studentDashboardController');
-const activityCtrl = require('../controllers/studentActivityController');
-const { authenticateStudent } = require('../middleware/studentAuth');
+const ctrl = require('../../controllers/studentDashboardController');
+const activityCtrl = require('../../controllers/studentActivityController');
+const { authenticateStudent } = require('../../middleware/studentAuth');
 
 router.use(authenticateStudent);
 router.get('/', ctrl.getDashboard);
