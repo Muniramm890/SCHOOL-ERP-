1716c52bd10a58d8e1a0d9bc98b56f8d02a9aa82
Mani <muniramm890@gmail.com>
2026-09-12 16:35:03 +0530

    Add payroll route to server

--- src/server.js
+++ src/server.js
@@ -87,6 +87,7 @@ app.use(`${API}/setup`,         require('./routes/setup'));
 app.use(`${API}/students`,      require('./routes/students'));
 app.use(`${API}/teachers`,      require('./routes/teachers'));
 app.use(`${API}/fees`,          require('./routes/fees'));
+app.use(`${API}/payroll`,       require('./routes/payroll'));
 app.use('/api/payments', require('./routes/payments'));
 app.use(`${API}/results`,       require('./routes/results'));
 app.use(`${API}/timetable`,    require('./routes/timetables'));
